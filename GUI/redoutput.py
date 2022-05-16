1c1d32fd7f5d04d8a7837cc82a9a1bffa449df18
Ignas Pakamore <pakamore.ignas2@gmail.com>
2022-05-16 09:30:14 +0100

    GUI

---
+++ GUI/redoutput.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import os
+
+root = Tk()
+termf = Frame(root, height=400, width=500)
+
+termf.pack(fill=BOTH, expand=YES)
+wid = termf.winfo_id()
+os.system('xterm -into %d -geometry 40x20 -sb &' % wid)
+
+
+root.mainloop()
