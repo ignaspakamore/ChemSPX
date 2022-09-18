d8fbd8d747e176a056324559f8e0bb53feb62814
Ignas Pakamore <pakamore.ignas2@gmail.com>
2022-09-18 10:19:57 +0100

    updated

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ setup(
     version='1.0',
     author='Ignas Pakamore',
     description='Chemical Space Explorer',
-    long_description='',
+    long_description='Algorithm for drawing spamples from a multi dimensional parameter space and its assessment.',
+    maintainer='Ignas Pakamore',
     url='https://github.com/ignaspakamore/ChemSPX',
     python_requires='>=3.7, <4',
+    license='MIT',
+    keywords=['parameter space', 'chemical space', 'reaction space'],
     install_requires=[
         'numpy',
         'sklearn',
